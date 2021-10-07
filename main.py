80383ce08f4add62edcc2e8cda82e17e4c275175
arha83 <amirrhosseinali@gmail.com>
2021-10-07 20:16:44 +0330

    pub/sub test

--- main.py
+++ main.py
@@ -9,16 +9,23 @@ def onSubscribe(client, userdata, mid, granted_qos):
 def onDisconnect(client, userdata, rc):
     print('onDisconnect')
 def onMessage(client, userdata, message):
-    print('onMessage')
+    print('onMessage', str(message.payload.decode('utf-8')))
 
 
-client= mqtt.Client(client_id='arhaLap')
-client.on_connect    = onConnect
-client.on_disconnect = onDisconnect
-client.on_message    = onMessage
-client.on_subscribe  = onMessage
+client= mqtt.Client('arhaLap')
+client.on_connect= onConnect
+client.on_disconnect= onDisconnect
+client.on_message= onMessage
+client.on_subscribe= onSubscribe
 
+client.loop_start()
 
 client.connect('192.168.1.33')
-time.sleep(5)
+time.sleep(1)
+client.subscribe('atitle')
+time.sleep(1)
+client.publish('atitle', payload='Is this working?')
+time.sleep(1)
 client.disconnect()
+
+client.loop_stop()
