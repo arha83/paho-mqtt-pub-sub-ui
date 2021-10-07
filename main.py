496a4db70218223a990f88cdc3c245720af40a8e
arha83 <amirrhosseinali@gmail.com>
2021-10-07 19:49:49 +0330

    connecion test code

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import paho.mqtt.client as mqtt
+import time
 
 
 def onConnect(client, userdata, flags_dict, result):
-    pass
+    print('connected...?!', type(result))
 def onSubscribe(client, userdata, mid, granted_qos):
     pass
 def onDisconnect(client, userdata, rc):
-    pass
+    print('disconnecting')
 def onMessage(client, userdata, message):
     pass
 
 
 client= mqtt.Client(client_id='arhaLap')
-
-
 client.on_connect    = connected
 client.on_disconnect = disconnected
 client.on_message    = message
 client.on_subscribe  = subscribed
+
+print('Connecting')
+client.connect('192.168.1.33')
+print('Sleeping')
+time.sleep(5)
+print('disconnecting')
+client.disconnect()
