cd55fd2ddb215f216b7e1e11186934dbe187df84
arha83 <amirrhosseinali@gmail.com>
2021-10-07 19:38:38 +0330

    added code structure

---
+++ main.py
@@ -0,0 +1,11 @@
+import paho.mqtt.client as mqtt
+
+
+def onConnect(client, userdata, flags_dict, result):
+    pass
+def onSubscribe(client, userdata, mid, granted_qos):
+    pass
+def onDisconnect(client, userdata, rc):
+    pass
+def onMessage(client, userdata, message):
+    pass
