2125c1208040f432643c73b874bde8738aa18d25
arha83 <amirrhosseinali@gmail.com>
2021-10-08 09:57:17 +0330

    put publishing in a seprate thread

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from os import sep
 import paho.mqtt.client as mqtt
 import time
+import threading
+import random
+import config
 
 def onConnect(client, userdata, flags_dict, result):
     print('onConnect')
@@ -11,31 +14,41 @@ def onDisconnect(client, userdata, rc):
 def onMessage(client, userdata, message):
     print('message with title ',message.topic,': ', str(message.payload.decode('utf-8')), sep='')
 
+def publisher(t, i):
+    while True:
+        #any value for publishing e.g: sensor value.
+        #type of value can be specified with topic
+        #e.g: if topic == 'lightSensor': publish light sensor value
+        client.publish(t, str(random.randrange(10)))
 
-broker='192.168.1.33'
+        if i < 0: break
+        else: time.sleep(i)
 
-client= mqtt.Client('arhaLap')
-client.on_connect= onConnect
-client.on_disconnect= onDisconnect
-client.on_message= onMessage
-client.on_subscribe= onSubscribe
 
-client.loop_start()
-
-client.connect(broker)
-time.sleep(1)
-
-while True:
-    command= input()
-    if command == 'sub':
-        topic= input('topic: ')
-        client.subscribe(topic)
-    elif command == 'pub':
-        topic= input('topic: ')
-        message= input('message: ')
-        client.publish(topic, message)
-    elif command == 'quit': break
+if __name__ == '__main__':
     
+    broker='192.168.1.33'
+    client= mqtt.Client('arhalap')
+    client.on_connect= onConnect
+    client.on_disconnect= onDisconnect
+    client.on_message= onMessage
+    client.on_subscribe= onSubscribe
 
-client.disconnect()
-client.loop_stop()
+    client.loop_start()
+
+    client.connect(broker)
+    time.sleep(1)
+    while True:
+        command= input()
+        if command == 'sub':
+            topic= input('topic: ')
+            client.subscribe(topic)
+        elif command == 'pub':
+            topic= input('topic: ')
+            interval= int(input('interval: '))
+            t= threading.Thread(target=publisher, args=(topic, interval), daemon= True)
+            t.start()
+        elif command == 'quit': break
+
+    client.disconnect()
+    client.loop_stop()
